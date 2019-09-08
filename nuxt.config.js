9a1f8395766c441c344c6dbf50b4b578e9f3d9b8
Tsutomu Okumura <tom.zed39@gmail.com>
2019-09-08 19:07:29 +0700

    deploy用のコマンド登録

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,8 +14,11 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/sample-nuxt-vuetify/favicon.ico' }
     ]
+  },
+  router: {
+    base: '/nuxt-project-sample/'
   },
   /*
   ** Customize the progress-bar color
